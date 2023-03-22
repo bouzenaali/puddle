c1e665578869835c8be939781d5a22c92bc0c155
bouzenaali <Alibouzena16@gmail.com>
2023-03-22 18:53:58 +0100

    done for now

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 def index(request):
-    return render(request, 'core/index.html')
+    Items = Item.objects.filter(is_sold = False)[0:6]
+    Categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'Categpries' : Categories,
+        'items' : Items,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
