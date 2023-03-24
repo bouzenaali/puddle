d6643e7c5c0477c2a3f36bd570bab7d74c907369
bouzenaali <Alibouzena16@gmail.com>
2023-03-24 11:28:12 +0100

    added the templates of the item app

--- item/views.py
+++ item/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Item
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    return render(request, 'item/detail.html',{
+        'item': item,
+    })
--- puddle/urls.py
+++ puddle/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import index,contact
 
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('item/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
